fbce81a8d1263eada45cdcf38ec3380cb185b50d
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-12-23 22:42:40 +0100

    Implement config

--- rocketload/config.py
+++ rocketload/config.py
@@ -1,9 +1,18 @@
 from .util import error
+from json import loads
 
 _config_file = "/rocketload.json"
 
 def get_config() -> dict:
-    return _get_file_content()
+    # Get and parse config file
+    rawConfig = _get_file_content()
+    parsed = loads(rawConfig)
+
+    # Validate config
+    if 'pollInterval' not in parsed:
+        raise Exception("Key 'pollInterval' not found in config.")
+
+    return parsed
 
 def _get_file_content() -> str:
     try:
